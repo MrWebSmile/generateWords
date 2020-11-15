64213f17f21c8ab279a2fd38d4241092999d7810
MrWebSmile <MrWebSmile@users.noreply.github.com>
2020-11-15 18:09:56 +0100

    update utili.js

--- ressources/scripts/utils.js
+++ ressources/scripts/utils.js
@@ -1,6 +1,6 @@
 function  getFile(){
 var file = "https://mrwebsmile.github.io/generateWords/ressources/divers/liste_francais.txt"
    fetch(file).then(function(response){
-       console.log(response.text)
+       console.log(response.blob)
    });
 }
