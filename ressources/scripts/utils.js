1745b94b798137d07568a8265c4e0f2bb53c673f
MrWebSmile <MrWebSmile@users.noreply.github.com>
2020-11-15 18:08:17 +0100

    modif index

--- ressources/scripts/utils.js
+++ ressources/scripts/utils.js
@@ -1,5 +1,5 @@
 function  getFile(){
-var file = "../ressources/divers/liste_francais.txt"
+var file = "https://mrwebsmile.github.io/generateWords/ressources/divers/liste_francais.txt"
    fetch(file).then(function(response){
        console.log(response.text)
    });
