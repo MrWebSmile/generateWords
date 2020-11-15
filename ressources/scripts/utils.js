37fb0e7a042d2786204509fe86db33b3c0addcaf
MrWebSmile <MrWebSmile@users.noreply.github.com>
2020-11-15 18:11:20 +0100

    modify utils

--- ressources/scripts/utils.js
+++ ressources/scripts/utils.js
@@ -1,6 +1,6 @@
 function  getFile(){
 var file = "https://mrwebsmile.github.io/generateWords/ressources/divers/liste_francais.txt"
    fetch(file).then(function(response){
-       console.log(response.blob)
+       console.log(response.blob())
    });
 }
