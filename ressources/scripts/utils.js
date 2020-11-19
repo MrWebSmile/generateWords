f03ff052945692a8e5009d9ee766d10f35a74422
MrWebSmile <MrWebSmile@users.noreply.github.com>
2020-11-19 10:14:32 +0100

    Update utils.js

--- ressources/scripts/utils.js
+++ ressources/scripts/utils.js
@@ -1,13 +1,23 @@
 function  getFile(){
 
-   var file = "https://mrwebsmile.github.io/generateWords/ressources/divers/liste_francais.txt";
+   const file = "https://mrwebsmile.github.io/generateWords/ressources/divers/liste_francais.txt";
 
    fetch(file).then(function(response){
        return response.text();
    })
    .then(function(data){
        //console.log(data);
-       var tab = data.split('\n');
+       let tab = data.split('\n');
        console.log(tab);
    });
+}
+
+function getRandomNumber(long,n){
+    let rand = -1;
+    let randTable =[];
+    for(var i = 0; i<n;i++){
+       rand = Math.floor(Math.random()*(long-0)+0);
+       randTable.push(rand);
+    }
+    return randTable;
 }
