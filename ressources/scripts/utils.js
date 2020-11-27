e986a44230e1dfddf2768d7c67e456e8960ca255
MrWebSmile <MrWebSmile@users.noreply.github.com>
2020-11-27 11:51:22 +0100

    Update utils.js

--- ressources/scripts/utils.js
+++ ressources/scripts/utils.js
@@ -40,5 +40,8 @@ function getRandomNumber(long,n){
 function deleteChild(){
     let wordsDiv = document.getElementById("words");
     let p = wordsDiv.querySelector('p');
-    let del = wordsDiv.removeChild(p);
+    if(p != 'undsefined'){
+        let del = wordsDiv.removeChild(p);
+    }
+    
 }
